aede39bb0bf466eca090db7210eea68fdcce31fa
grisa <grisha.maryin@gmail.com>
2023-12-25 19:33:09 +0500

    +new screens and transition from start window to others

--- game_screen.py
+++ game_screen.py
@@ -5,9 +5,9 @@ from constants import WIDTH, HEIGHT, FPS, STEP_TEXT, INDENT, SIZE
 from terminate import terminate
 from util import load_image
 
-def settings_screen(screen):
-    intro_text = ["Настройки"]
-    fon = pygame.transform.scale(load_image('light_purple.png'), (WIDTH, HEIGHT))
+def start_game(screen):
+    intro_text = ["Игра"]
+    fon = pygame.transform.scale(load_image('Sky_Blue.png'), (WIDTH, HEIGHT))
     screen.blit(fon, (0, 0))
     font = pygame.font.Font(None, 60)
     text_coord = INDENT
@@ -18,5 +18,6 @@ def settings_screen(screen):
         intro_rect.x = (WIDTH - intro_rect.width) // 2
         text_coord += intro_rect.height + STEP_TEXT
         screen.blit(string_rendered, intro_rect)
+
 
 
